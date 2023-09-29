2f40abb39a2d6722c0d439d7bb2f3f896f9ed981
Lindternal <den_080202@mail.ru>
2023-09-30 01:25:20 +0300

    updated to 1.20.2

--- src/main/java/com/linda/simplehardcore/SimpleHardcore.java
+++ src/main/java/com/linda/simplehardcore/SimpleHardcore.java
@@ -17,7 +17,7 @@ public final class SimpleHardcore extends JavaPlugin {
     @Override
     public void onEnable() {
         getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Simple Hardcore has been enabled!");
-        getServer().getConsoleSender().sendMessage(ChatColor.AQUA + "Simple Hardcore for 1.20.1 made by Lindternal and FallenVEye");
+        getServer().getConsoleSender().sendMessage(ChatColor.AQUA + "Simple Hardcore for 1.20.2 made by Lindternal and FallenVEye");
         getServer().getPluginManager().registerEvents(new DeathMessage(this), this);
         getServer().getPluginManager().registerEvents(new LoginLogout(this), this);
         getServer().getPluginManager().registerEvents(new ShopGUI(), this);
